0cb63453e6f2b4d6a89b28595154336b7020f695
okawa-h <ookawa124@gmail.com>
2019-02-28 09:24:51 +0900

    fix free triangle

--- free-triangle/files/js/script.js
+++ free-triangle/files/js/script.js
@@ -32,49 +32,50 @@ import { Setting } from '../../../common/files/js/setting.js';
 
 	class Triangle {
 
-		constructor(size,x,y,color) {
+		constructor(radius,x,y,color) {
 
-			this.size       = size;
+			this.radius     = radius;
 			this.color      = color;
 			this.center     = new Point(x,y);
 			this.vertexs    = this.createVertexs(3);
-			this.velocity   = getRangeNumber(1,3);
 
 			this.angle      = getRangeNumber(0,360);
 			this.speedAngle = getRangeNumber(0.1,3);
 
+			this.velocity   = this.getRandomVelocity();
 			this.direction  = this.getRandomDirection();
 
 		}
 
 		update(minX,minY,maxX,maxY) {
 
-			if (maxX <= this.center.x + this.size * .5) {
+			if (maxX <= this.center.x + this.radius) {
 				this.direction.x = -1;
-				this.velocity   = getRangeNumber(1,3);
+				this.velocity = this.getRandomVelocity();
 			}
 
-			if (this.center.x - this.size * .5 <= minX) {
+			if (this.center.x - this.radius <= minX) {
 				this.direction.x = 1;
-				this.velocity   = getRangeNumber(1,3);
+				this.velocity = this.getRandomVelocity();
 			}
 
-			if (maxY <= this.center.y + this.size * .5) {
+			if (maxY <= this.center.y + this.radius) {
 				this.direction.y = -1;
-				this.velocity   = getRangeNumber(1,3);
+				this.velocity = this.getRandomVelocity();
 			}
 
-			if (this.center.y - this.size * .5 <= minY) {
+			if (this.center.y - this.radius <= minY) {
 				this.direction.y = 1;
-				this.velocity   = getRangeNumber(1,3);
+				this.velocity = this.getRandomVelocity();
 			}
 
-			this.velocity *= 1.01;
+			this.velocity.x *= 1.05;
+			this.velocity.y *= 1.05;
 
 			this.angle += this.speedAngle;
 
-			this.center.x += this.velocity * this.direction.x;
-			this.center.y += this.velocity * this.direction.y;
+			this.center.x += this.velocity.x * this.direction.x;
+			this.center.y += this.velocity.y * this.direction.y;
 
 			if (360 <= this.angle) this.angle = 0;
 
@@ -108,14 +109,14 @@ import { Setting } from '../../../common/files/js/setting.js';
 
 		createVertexs(length) {
 
-			const ratioSize = this.size * .3;
+			const ratioSize = this.radius * .3;
 			const distanse  = 360 / length;
 			let points = [];
 
 			for (let i = 0; i < length; i++) {
 
 				const randomDistanse = distanse + getRangeNumber(-ratioSize,ratioSize);
-				const point = this.getCircleOnPoint(this.size,randomDistanse * i);
+				const point = this.getCircleOnPoint(this.radius,randomDistanse * i);
 				points.push(point);
 
 			}
@@ -126,8 +127,8 @@ import { Setting } from '../../../common/files/js/setting.js';
 
 		getCircleOnPoint(radius,angle) {
 
-			let x = radius * Math.cos(angle * (Math.PI / 180));
-			let y = radius * Math.sin(angle * (Math.PI / 180));
+			const x = radius * Math.cos(angle * (Math.PI / 180));
+			const y = radius * Math.sin(angle * (Math.PI / 180));
 			return new Point(x,y);
 
 		}
@@ -137,6 +138,15 @@ import { Setting } from '../../../common/files/js/setting.js';
 			return {
 				x:[-1,1][Math.floor(Math.random()*2)],
 				y:[-1,1][Math.floor(Math.random()*2)]
+			}
+
+		}
+
+		getRandomVelocity() {
+
+			return {
+				x:getRangeNumber(.1,5),
+				y:getRangeNumber(.1,5)
 			}
 
 		}
@@ -190,14 +200,13 @@ import { Setting } from '../../../common/files/js/setting.js';
 		_objects = [];
 		const width  = _canvas.width;
 		const height = _canvas.height;
-
-		const size = _setting.get('size');
+		const size   = _setting.get('size');
 
 		for (let i = 0; i < _setting.get('length'); i++) {
 
-			const color = getRandomColor();
-
-			_objects.push(new Triangle(size,width*.5,height*.5,color));
+			const color  = getRandomColor();
+			const radius = getRangeNumber(size - size * .9,size + size * .9);
+			_objects.push(new Triangle(radius,width*.5,height*.5,color));
 
 		}
 
